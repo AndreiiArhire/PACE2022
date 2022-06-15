efb60b7620eeb7ac505e811d8b723cb0627ba482
Andrei <andreiarhire2708@gmail.com>
2022-06-15 16:41:58 +0300

    prepare release

---
+++ pace-2022/cmake-build-debug/CMakeFiles/pace_2022.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pace_2022.dir/main.cpp.obj"
+  "libpace_2022.dll.a"
+  "pace_2022.exe"
+  "pace_2022.exe.manifest"
+  "pace_2022.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pace_2022.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
